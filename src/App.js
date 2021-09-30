f8bf96ae36179d63f71300ca34027fba3017c85f
Maseko <Nkululeko.Maseko@vodacom.co.za>
2021-09-30 14:19:08 +0200

    Second Commit, added src files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
+import Trending from './pages/Trending/Trending';
+import Movies from './pages/Movies/Movies';
+import Series from './pages/Series/Series';
+import Search from './pages/Search/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path='/' component={Trending} exact/>
+            <Route path='/movies' component={Movies}></Route>
+            <Route path='/series' component={Series}></Route>
+            <Route path='/search' component={Search}></Route>
+          </Switch>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
